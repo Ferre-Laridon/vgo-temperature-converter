42c2df3d6e026ea5ef3fb87c44892f08bdeccbcc
Ferre Laridon <ferre.laridon@student.ucll.be>
2023-04-08 14:52:45 +0200

    made it so the conversion between temperatures is based on an interface and not duplicate code

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -24,48 +24,6 @@ namespace View
         }
     }
 
-    public class CelsiusConverter : IValueConverter
-    {
-        private double kelvinNumber = 273.15;
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            var celsius = kelvin - kelvinNumber;
-
-            return celsius.ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var celsius = double.Parse((string)value);
-            var kelvin = celsius + kelvinNumber;
-
-            return kelvin;
-        }
-    }
-
-    public class FahrenheitConverter : IValueConverter
-    {
-        private double kelvinNumber = 273.15;
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            var fahrenheit = (kelvin - kelvinNumber) / (5.0 / 9) + 32;
-
-            return fahrenheit;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var fahrenheit = double.Parse((string)value);
-            var kelvin = (fahrenheit - 32) / (5.0 / 9) + kelvinNumber;
-
-            return kelvin;
-        }
-    }
-
     public class TemperatureConverter : IValueConverter
     {
         public ITemperatureScale TemperatureScale { get; set; }
