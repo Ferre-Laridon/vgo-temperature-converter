4b11c925be39ef4341b6ecd61ff732416fe944e2
Ferre Laridon <ferre.laridon@student.ucll.be>
2023-04-08 15:16:39 +0200

    added observer to MainWindow so the slider updates the textboxes

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -16,8 +18,25 @@ using System.Windows.Shapes;
 
 namespace View
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double temperatureInKelvin;
+
+        public double TemperatureInKelvin
+        {
+            get
+            {
+                return temperatureInKelvin;
+            }
+            set
+            {
+                temperatureInKelvin = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
